65d8bfe6cde021c8b23e875ee009467907e1348e
BWooRam <wrbae@hyundaiht.co.kr>
2024-03-20 09:30:01 +0900

    - Core : BaseComposeView 주석 추가

--- core/designsystem/src/main/java/com/example/sampleapp/core/designsystem/component/BaseComposeView.kt
+++ core/designsystem/src/main/java/com/example/sampleapp/core/designsystem/component/BaseComposeView.kt
@@ -2,6 +2,9 @@ package com.example.sampleapp.core.designsystem.component
 
 import androidx.compose.runtime.Composable
 
+/**
+ * Compose 그리기 관련 Interface
+ */
 interface BaseComposeView {
     @Composable
     fun OnDraw()
